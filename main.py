1212641ee79727144e8347f79fb3c8b17d03da8b
huckmon <45248261+huckmon@users.noreply.github.com>
2024-12-24 16:56:08 +1030

    GET request functioning

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class HTTP_server:
 
     http_ver = "HTTP/1.1 "
 
-    def __init__(self, host='127.0.0.1', port=10000):
+    def __init__(self, host='127.0.0.1', port=10002):
         self.host = host
         self.port = port
 
@@ -26,9 +26,13 @@ class HTTP_server:
 
             data = connection.recv(1024)
 
-            response = self.request_handler(data)
+            #if data:
+            response_message = self.request_handler(data)
 
-            #connection.sendall(response)
+            connection.sendall(response_message)
+
+            #else:
+
             connection.close()
 
 
@@ -51,9 +55,10 @@ class HTTP_server:
             # probably want to replace this with regex queries later so that it's not so scuffed
             for x in range(len(request_start_line)):
                 if (request_start_line[x:(x+8)] == "HTTP/1.1"):
-                    # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory
+                    # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory+ "; charset=UTF-8"
                     request_target = "." + request_start_line[4:(x-1)]
-                    print("request-target is", request_target)
+
+                    #print("request-target is", request_target)
 
             # if pointing at root, redirect to index file
             if (request_target == "./"):
@@ -61,10 +66,10 @@ class HTTP_server:
                 request_target = "./index.html"
 
                 request_target_file = open(request_target, "r")
-                content_length = sys.getsizeof(request_target_file)
+                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
 
                 # get the content type and saves as a tuple natively, don't need to modify as can just take part of tuple
-                self.get_content_mime_type(request_target)
+                #self.get_content_mime_type(request_target)
 
                 response_status_code = "200 OK"
 
@@ -74,22 +79,41 @@ class HTTP_server:
             else:
                 try:
                     request_target_file = open(request_target, "r")
-                    content_length = sys.getsizeof(request_target_file)
+                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
 
-                    self.get_content_mime_type(request_target)
+                    #self.get_content_mime_type(request_target)
 
                     response_status_code = "200 OK"
                 except:
                     response_status_code = "404 Not Found"
 
-
-            response_start_line = http_ver + response_status_code
-            print("response start line is", response_start_line)
-
-            date_header = datetime.datetime.now().strftime("%a, %d, %b, %Y, %H:%M:%S GMT")
-            print(date_header)
+                    # change the request_target_file to the stock 404 file to avoid renaming variables or adding checks
+                    request_target_file = open("./404-page.html", "r")
+                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
 
 
+            response_start_line = self.http_ver + response_status_code
+
+            date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
+
+            content_type_header = self.get_content_mime_type(request_target)
+
+            temp_response = response_start_line + "\r\n" + date_header + "\r\n" + content_length_header + "\r\n" + content_type_header + "\r\n"
+
+            http_page_line_array = request_target_file.read().splitlines()
+
+            http_page_line = ""
+
+            for x in range(len(http_page_line_array)):
+                http_page_line = http_page_line + "\r\n" + http_page_line_array[x]
+
+
+            temp_response_bytes = temp_response.encode(encoding="utf-8")
+            http_page_lines_bytes = http_page_line.encode(encoding="utf-8")
+
+            response_message = b"".join([temp_response_bytes, http_page_lines_bytes])
+
+            return response_message
 
 
 
@@ -106,10 +130,12 @@ class HTTP_server:
     def get_content_mime_type(self, request_target):
         # quick function to get the mime type and then throw it back so this isn't repeated multiple times'
         content_type = mimetypes.guess_type(request_target)
-        print("mime type of request-target is", content_type[0])
-        content_type = content_type[0]
-        return content_type
 
+        #print("mime type of request-target is", content_type[0])
+
+        content_type_header = "Content-Type: " + str(content_type[0])
+        print(content_type_header)
+        return content_type_header
 
 
 if __name__ == '__main__':
