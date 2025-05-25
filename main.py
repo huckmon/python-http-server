8e5f5bc15befacc5ba4bdb73398044d7c6c418b8
Huck <45248261+huckmon@users.noreply.github.com>
2025-05-25 20:02:01 +0930

    Update main.py
    - Moved to case statements in request_handler
    - Moved request method processing into own functions

--- main.py
+++ main.py
@@ -66,147 +66,181 @@ class HTTP_server:
                 print('Current request method: ' + current_request_method)
                 break
 
-        if ("GET" in current_request_method):
-            print("GET Request Confirmed")
+        # match case sorts through the request methods and then goes to specific function for method
+        match current_request_method:
+            case "GET":
+                response = self.get_method_received(request_start_line)
+                return response
+            case "HEAD":
+                response = self.head_method_received(request_start_line)
+                return response
+            case "OPTIONS":
+                response = self.options_method_received(request_start_line)
+                return response
+            case "POST":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case "PUT":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case "DELETE":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case "CONNECT":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case "TRACE":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case "PATCH":
+                response = self.not_implemented_response(request_start_line)
+                return response
+            case _:
+                response = self.invalid_request_method(request_start_line)
+                return response
 
-            # loop through and seperate the request-target to use later in finding resource
-            # probably want to replace this with regex queries later so that it's not so scuffed
-            for x in range(len(request_start_line)):
-                if (request_start_line[x:(x+8)] == "HTTP/1.1"):
-                    # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory
-                    request_target = "." + request_start_line[4:(x-1)]
-                    break
+    # Function for receiving GET methods
+    def get_method_received(self, request_start_line):
+        print("GET Request Confirmed")
 
-            # if pointing at root, redirect to index file
-            if (request_target == "./"):
-                # manually set the request target as the index page so there's a default page
-                request_target = "./index.html"
+        # loop through and seperate the request-target to use later in finding resource
+        # probably want to replace this with regex queries later so that it's not so scuffed
+        print('request_start_line ' + request_start_line)
+        for x in range(len(request_start_line)):
+            if (request_start_line[x:(x+8)] == "HTTP/1.1"):
+                # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory
+                request_target = "." + request_start_line[4:(x-1)]
+                break
 
-                request_target_file = open(request_target, "r")
-                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-                response_status_code = "200 OK"
+        # if pointing at root, redirect to index file
+        if (request_target == "./"):
+            # manually set the request target as the index page so there's a default page
+            request_target = "./index.html"
 
-            # no reason to call any but else since can just use except to throw a 400 or 404
-            # handles non-root and invalid path requests
-            else:
-                try:
-                    request_target_file = open(request_target, "r")
-                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-                    response_status_code = "200 OK"
-                except:
-                    # The reuested item wasn't found, return a 404 respose
-                    response_status_code = "404 Not Found"
+            request_target_file = open(request_target, "r")
+            content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
+            response_status_code = "200 OK"
 
-                    # change the request_target file to the stock 404 file to avoid renaming variables or adding checks. Allows users to edit 404 page
-                    request_target_file = open("./404-page.html", "r")
-                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-
-            response_start_line = self.HTTP_VER + response_status_code
-            date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
-            content_type_header = self.get_content_mime_type(request_target)
-            temp_response = response_start_line + "\r\n" + date_header + "\r\n" + content_length_header + "\r\n" + content_type_header + "\r\n"
-
-            http_page_line_array = request_target_file.read().splitlines()
-            http_page_line = ""
-
-            # loops through the lines in the request_target_file as an array
-            for x in range(len(http_page_line_array)):
-                http_page_line = http_page_line + "\r\n" + http_page_line_array[x]
-
-            temp_response_bytes = temp_response.encode(encoding="utf-8")
-            http_page_lines_bytes = http_page_line.encode(encoding="utf-8")
-            response_message = b"".join([temp_response_bytes, http_page_lines_bytes])
-
-            return response_message
-
-
-
-        elif ("HEAD" in current_request_method):
-            print("HEAD request confirmed")
-
-            # loop through and seperate the request-target to use later in finding resource
-            # probably want to replace this with regex queries later so that it's not so scuffed
-            for x in range(len(request_start_line)):
-                if (request_start_line[x:(x+8)] == "HTTP/1.1"):
-                    # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory"
-                    request_target = "." + request_start_line[5:(x-1)]
-                    break
-
-            # if pointing at root, redirect to index file
-            if (request_target == "./"):
-                # manually set the request target as the index page so there's a default page
-                request_target = "./index.html"
-
-                request_target_file = open(request_target, "r")
-                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-                response_status_code = "200 OK"
-
-            # no reason to call any but else since can just use except to throw a 400 or 404
-            # handles non-root and invalid path requests
-            else:
-                try:
-                    request_target_file = open(request_target, "r")
-                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-                    response_status_code = "200 OK"
-                except:
-                    response_status_code = "404 Not Found"
-
-                    # change the request_target_file to the stock 404 file to avoid renaming variables or adding checks
-                    request_target_file = open("./404-page.html", "r")
-                    content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
-
-            response_start_line = self.HTTP_VER + response_status_code
-            date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
-            content_type_header = self.get_content_mime_type(request_target)
-
-            temp_response = response_start_line + "\r\n" + date_header + "\r\n" + content_length_header + "\r\n" + content_type_header + "\r\n"
-            response_message = temp_response.encode(encoding="utf-8")
-
-            return response_message
-
-
-        elif ("OPTIONS" in current_request_method):
-            print("OPTIONS request confirmed")
-
-            response_status_code = "204 No Content"
-            response_start_line = self.HTTP_VER + response_status_code
-            allowed_option_header = "OPTIONS, HEAD, GET"
-            date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
-
-            temp_response = response_start_line + "\r\n" + allowed_option_header + "\r\n" + date_header + "\r\n"
-            response_message = temp_response.encode(encoding="utf-8")
-
-            return response_message
-
-        # list all other request methods and add a 501 response NOTE I hate these large if statments, try moving to switch statements
-        # the array mentions are because I found it easier to just check if that word exactly is there. NOTE might be easer
-        elif (("POST" in current_request_method) or ("PUT" in current_request_method) or ("DELETE" in current_request_method) or ("CONNECT" in current_request_method) or ("TRACE" in current_request_method) or ("PATCH" in current_request_method)):
-
-            response_status_code = "501 Not Implemented"
-            response_start_line = self.HTTP_VER + response_status_code
-            response_message = response_start_line.encode(encoding="utf-8")
-
-            return response_message
-
+        # no reason to call any but else since can just use except to throw a 400 or 404
+        # handles non-root and invalid path requests
         else:
-            print("Invalid request recieved")
+            try:
+                request_target_file = open(request_target, "r")
+                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
+                response_status_code = "200 OK"
+            except:
+                # The reuested item wasn't found, return a 404 respose
+                response_status_code = "404 Not Found"
 
-            response_status_code = "400 Bad Request"
-            response_start_line = self.HTTP_VER + response_status_code
-            body_response = b"""
-            {
-                "error": "Bad Request",
-                "message": "Request body could not be read properly"
-            }
-            """
+                # change the request_target file to the stock 404 file to avoid renaming variables or adding checks. Allows users to edit 404 page
+                request_target_file = open("./404-page.html", "r")
+                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
 
-            temp_response = response_start_line + "\r\n"
-            temp_response_bytes = temp_response.encode(encoding="utf-8")
-            response_message = b"".join([temp_response_bytes, body_response])
+        response_start_line = self.HTTP_VER + response_status_code
+        date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
+        content_type_header = self.get_content_mime_type(request_target)
+        temp_response = response_start_line + "\r\n" + date_header + "\r\n" + content_length_header + "\r\n" + content_type_header + "\r\n"
 
-            return response_message
+        http_page_line_array = request_target_file.read().splitlines()
+        http_page_line = ""
 
+        # loops through the lines in the request_target_file as an array
+        for x in range(len(http_page_line_array)):
+            http_page_line = http_page_line + "\r\n" + http_page_line_array[x]
 
+        temp_response_bytes = temp_response.encode(encoding="utf-8")
+        http_page_lines_bytes = http_page_line.encode(encoding="utf-8")
+        response_message = b"".join([temp_response_bytes, http_page_lines_bytes])
+
+        return response_message
+
+    # Function when receiving HEAD methods
+    def head_method_received(self, request_start_line):
+        print("HEAD request confirmed")
+
+        # loop through and seperate the request-target to use later in finding resource
+        # probably want to replace this with regex queries later so that it's not so scuffed
+        for x in range(len(request_start_line)):
+            if (request_start_line[x:(x+8)] == "HTTP/1.1"):
+                # add a dot so that request-target is seached for so the http server can be placed in somewhere other than root directory"
+                request_target = "." + request_start_line[5:(x-1)]
+                break
+
+        # if pointing at root, redirect to index file
+        if (request_target == "./"):
+            # manually set the request target as the index page so there's a default page
+            request_target = "./index.html"
+
+            request_target_file = open(request_target, "r")
+            content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
+            response_status_code = "200 OK"
+
+        # no reason to call any but else since can just use except to throw a 400 or 404
+        # handles non-root and invalid path requests
+        else:
+            try:
+                request_target_file = open(request_target, "r")
+                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
+                response_status_code = "200 OK"
+            except:
+                response_status_code = "404 Not Found"
+
+                # change the request_target_file to the stock 404 file to avoid renaming variables or adding checks
+                request_target_file = open("./404-page.html", "r")
+                content_length_header = "Content-Length: " + str(sys.getsizeof(request_target_file))
+
+        response_start_line = self.HTTP_VER + response_status_code
+        date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
+        content_type_header = self.get_content_mime_type(request_target)
+
+        temp_response = response_start_line + "\r\n" + date_header + "\r\n" + content_length_header + "\r\n" + content_type_header + "\r\n"
+        response_message = temp_response.encode(encoding="utf-8")
+
+        return response_message
+
+    # Function for receiving OPTIONS methods
+    def options_method_received(self, request_start_line):
+        print("OPTIONS request confirmed")
+
+        response_status_code = "204 No Content"
+        response_start_line = self.HTTP_VER + response_status_code
+        allowed_option_header = "OPTIONS, HEAD, GET"
+        date_header = datetime.datetime.now().strftime("Date: %a, %d, %b, %Y, %H:%M:%S GMT")
+
+        temp_response = response_start_line + "\r\n" + allowed_option_header + "\r\n" + date_header + "\r\n"
+        response_message = temp_response.encode(encoding="utf-8")
+
+        return response_message
+
+    # Function for recieving unimplemented methods
+    def not_implemented_response(self, request_start_line):
+
+        response_status_code = "501 Not Implemented"
+        response_start_line = self.HTTP_VER + response_status_code
+        response_message = response_start_line.encode(encoding="utf-8")
+
+        return response_message
+
+    # Function for returning a bad request method/400 response code
+    def invalid_request_method(self, request_start_line):
+        print("Invalid request recieved")
+
+        response_status_code = "400 Bad Request"
+        response_start_line = self.HTTP_VER + response_status_code
+        body_response = b"""
+        {
+            "error": "Bad Request",
+            "message": "Request body could not be read properly"
+        }
+        """
+
+        temp_response = response_start_line + "\r\n"
+        temp_response_bytes = temp_response.encode(encoding="utf-8")
+        response_message = b"".join([temp_response_bytes, body_response])
+
+        return response_message
+
+    # function gets the MIME type of input
     def get_content_mime_type(self, request_target):
         # quick function to get the mime type and then throw it back so this isn't repeated multiple times'
         content_type = mimetypes.guess_type(request_target)
@@ -216,7 +250,6 @@ class HTTP_server:
         content_type_header = "Content-Type: " + str(content_type[0])
         print(content_type_header)
         return content_type_header
-
 
 if __name__ == '__main__':
     # start the http server when script is executed
